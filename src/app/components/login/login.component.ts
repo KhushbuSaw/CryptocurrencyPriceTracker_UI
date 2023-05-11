dfcb407cbe12e116318a13b51aa885ec95c6fcb9
Khushbu <lavsingh@exatainsolutions.com>
2023-05-11 13:16:52 +0530

    validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,43 @@ export class LoginComponent implements OnInit{
   type:string='password'
   isText:boolean=false;
   eyeIcon:string="fa-eye-slash";
-  constructor() { }
+  loginForm!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-  
+  this.loginForm=this.fb.group({
+    username:['',Validators.required],
+    password:['',Validators.required],
+  })
   }
   hideShowPass(){
    this.isText=!this.isText;
    this.isText?this.eyeIcon="fa-eye" : this.eyeIcon="fa-eye-slash";
    this.isText?this.type="text":this.type="password";
+  }
+  onSubmit()
+  {
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+
+    }
+    else{
+      this.validateAllFormFileds(this.loginForm);
+      alert("this.loginForm.value");
+    }
+  }
+
+  private validateAllFormFileds(formGroup:FormGroup)
+  {
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+      control.markAsDirty({onlySelf:true});
+      }
+      else if(control instanceof FormGroup){
+        this.validateAllFormFileds(control)
+      }
+    })
   }
 
 }
