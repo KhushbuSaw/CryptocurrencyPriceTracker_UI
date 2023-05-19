1cf1b2545375c3c3be2a0e1f846422495c351391
Khushbu Saw <khushbusaw76@gmail.com>
2023-05-19 22:01:15 +0530

    authorization

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,24 +9,33 @@ export class AuthService {
 
   private baseUrl:string="http://localhost:40812/api/UserDetails/"
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router:Router) { }
 
   signUp(userObj:any){
-    return this.http.post<any>(`${this.baseUrl}register`,userObj)
+    return this.http.post<any>(`${this.baseUrl}register`,userObj);
   }
   login(loginObj:any)
   {
-    return this.http.post<any>(`${this.baseUrl}authenticate`,loginObj)
+    return this.http.post<any>(`${this.baseUrl}authenticate`,loginObj);
+  }
+  getUsers()
+  {
+    return this.http.get<any>(this.baseUrl);
+  }
+  signOut()
+  {
+    localStorage.clear();
+    this.router.navigate(['login']);
   }
   storeToken(tokenValue:string)
   {
-    localStorage.setItem('token',tokenValue)
+    localStorage.setItem('token',tokenValue);
   }
   getToken()
   {
-    return localStorage.getItem('token')
+    return localStorage.getItem('token');
   }
   isLoggedIn():boolean{
-    return !!localStorage.getItem('token')
+    return !!this.getToken();
   }
 }
