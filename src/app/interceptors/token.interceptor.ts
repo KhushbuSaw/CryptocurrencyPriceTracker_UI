73afa10d1209d93f9c6f7340b22141fe24b0a2c5
Khushbu Saw <khushbusaw76@gmail.com>
2023-05-24 17:30:09 +0530

    refresh Token

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -6,9 +6,10 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../Services/auth.service';
 import { Router } from '@angular/router';
+import { TokenDtoModel } from '../models/TokenDtoModel';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -27,12 +28,33 @@ export class TokenInterceptor implements HttpInterceptor {
       catchError((err:any)=>{
         if(err instanceof HttpErrorResponse){
           if(err.status===401){
-            alert("Token is Expired,Login again");
-            this.route.navigate(['login'])
+            return this.handleUnAuthorizedError(request,next)
           }
         }
         return throwError(()=>new Error("Some other error occured"))
       })
   );
+  }
+  handleUnAuthorizedError(req:HttpRequest<any>,next:HttpHandler){
+    let tokenDtoModel=new TokenDtoModel();
+    tokenDtoModel.accessToken=this.auth.getToken()!;
+    tokenDtoModel.refreshToken=this.auth.getRefreshToken()!;
+    return this.auth.renewToken(tokenDtoModel)
+    .pipe(
+      switchMap((data:TokenDtoModel)=>{
+        this.auth.storeRefreshToken(data.refreshToken);
+        this.auth.storeToken(data.accessToken);
+        req=req.clone({
+          setHeaders: {Authorization:`bearer ${data.accessToken}`}
+        })
+        return next.handle(req)
+      }),
+      catchError((err)=>{
+        return throwError(()=>{
+          alert("Token is Expired,Login again");
+          this.route.navigate(['login'])
+        })
+      })
+    )
   }
 }
