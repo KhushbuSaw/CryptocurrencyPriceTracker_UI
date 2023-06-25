35b615d151593a10cf3226d1c27d21c404f2784b
Khushbu Saw <khushbusaw76@gmail.com>
2023-06-25 20:47:49 +0530

    added name in dashboard

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/Services/auth.service';
 import { CurrencyService } from 'src/app/Services/currency.service';
 
@@ -7,11 +7,29 @@ import { CurrencyService } from 'src/app/Services/currency.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   selectedCurrency:string="INR";
+  public fullName:string="";
+  public users:any=[];
   constructor(private currencyService:CurrencyService,private auth:AuthService)
   {}
-
+  ngOnInit() {
+    //  this.auth.getUsers()
+    //  .subscribe(res=>
+    //   {
+    //     this.users=res;
+    //   });
+       this.auth.getFullNameFromStore()
+      .subscribe(val=>{
+        let fullNameFromToken=this.auth.getFullNameFromToken();
+        this.fullName=val||fullNameFromToken
+      });
+    //   this.auth.getRoleFromStore()
+    //   .subscribe(val=>{
+    //     const roleFromToken=this.auth.getRoleFromToken();
+    //     this.role=val||roleFromToken;
+    //   })
+  }
   sendCurrency(event:string){
     this.currencyService.setCurrency(event);
     this.selectedCurrency=event;
